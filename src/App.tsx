2f0750a93c9c4392bacc993f7f39cf8bedf998e2
Melissa Jo Tully <150461964+MissTully@users.noreply.github.com>
2026-06-17 10:08:56 -0400

    Add onboarding and members routes with ProtectedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,34 @@ import Landing from "./pages/Landing";
 import Program from "./pages/Program";
 import Phase from "./pages/Phase";
 import NotFound from "./pages/NotFound";
+import Onboarding from "./pages/Onboarding";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/program" element={<Program />} />
-        <Route path="/program/:slug" element={<Phase />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Layout>
-  );
-}
+    return (
+          <Routes>
+                <Route element={<Layout />}>
+                        <Route path="/" element={<Landing />} />
+                        <Route path="/program" element={<Program />} />
+                        <Route path="/program/:slug" element={<Phase />} />
+                        <Route
+                                    path="/onboarding"
+                                    element={
+                                                  <ProtectedRoute requireOnboarding={false}>
+                                                                <Onboarding />
+                                                  </ProtectedRoute>ProtectedRoute>
+                          }
+                                />
+                                <Route
+                                            path="/members"
+                                            element={
+                                                          <ProtectedRoute>
+                                                                        <Program />
+                                                          </ProtectedRoute>ProtectedRoute>
+                                  }
+                                        />
+                                        <Route path="*" element={<NotFound />} />
+                                </Route>Route>
+                        </Route>Routes>
+                  );
+                  }</Routes>
