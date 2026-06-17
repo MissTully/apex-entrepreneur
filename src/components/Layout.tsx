d57f2a994b2b067ccaa0f620e1b3797d521d4cc9
Melissa Jo Tully <150461964+MissTully@users.noreply.github.com>
2026-06-17 10:42:30 -0400

    Fix formatting issues in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,12 +4,12 @@ import Footer from "./Footer";
 import MentorPanel from "./MentorPanel";
 
 export default function Layout() {
-    return (
-          <div className="flex min-h-screen flex-col">
-                <Navbar />
-                <main className="flex-1"><Outlet /></main>main>
-                <Footer />
-                <MentorPanel />
-          </div>div>
-        );
-}</div>
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Navbar />
+      <main className="flex-1"><Outlet /></main>
+      <Footer />
+      <MentorPanel />
+    </div>
+  );
+}
