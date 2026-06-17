930bfe0a9d6357356ed6c3b4ff15ac2a4d4701c1
Melissa Jo Tully <150461964+MissTully@users.noreply.github.com>
2026-06-17 10:11:23 -0400

    Update Layout to use Outlet for react-router nested routes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,15 @@
-import { ReactNode } from "react";
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import MentorPanel from "./MentorPanel";
 
-export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <Navbar />
-      <main className="flex-1">{children}</main>
-      <Footer />
-      <MentorPanel />
-    </div>
-  );
-}
+export default function Layout() {
+    return (
+          <div className="flex min-h-screen flex-col">
+                <Navbar />
+                <main className="flex-1"><Outlet /></main>main>
+                <Footer />
+                <MentorPanel />
+          </div>div>
+        );
+}</div>
